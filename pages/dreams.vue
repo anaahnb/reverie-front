4bc5797d6481727bb0779135ab5cd124decd7ecc
Ana Julia <alvncar@gmail.com>
2024-12-18 18:56:24 -0300

    feat login/index page

---
+++ pages/dreams.vue
@@ -0,0 +1,3 @@
+<template>
+    oiiiiiiiiiiiiiiiiiiiii
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="page">
+    <div class="container">
+      <div class="header">
+        <h1>Bem vindo ao <b>Reverie</b> — o diário do seus sonhos.</h1>
+        <p>Não possui uma conta? <a href="/register">Registre-se</a></p>
+      </div>
+      <form @submit.prevent="handleSubmit">
+        <div class="form_content">
+          <Input
+            v-for="input in form.inputs"
+            :key="input.id"
+            v-bind="input"
+            v-model="input.value"
+						:type="input.type"
+          />
+          <Button :disabled="isButtonDisabled"> Entrar </Button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import { login } from '~/api/auth';
+
+export default {
+  components: {},
+  data() {
+    return {
+      form: {
+        inputs: [
+          {
+            id: "email",
+            label: "Email",
+            placeholder: "Insira o seu email",
+            value: "",
+            error: "",
+						type: "email"
+          },
+          {
+            id: "password",
+            label: "Senha",
+            placeholder: "Insira a sua senha",
+            value: "",
+            error: "",
+						type: "password"
+          },
+        ],
+      },
+    };
+  },
+  computed: {
+    isButtonDisabled() {
+      if (!this.form.inputs[0].value || !this.form.inputs[1].value) {
+        return true;
+      }
+      return false;
+    },
+  },
+  methods: {
+		handleSubmit() {
+			const params = this.form.inputs.reduce((acc, input) => {
+				acc[input.id] = input.value
+				return acc
+			}, {})
+
+			login(params).then(res => {
+				localStorage.setItem('token', res.authorisation.token)
+				this.$router.push('/dreams')
+			})
+		},
+  },
+};
+</script>
+
+<style lang="scss">
+.page {
+  min-height: 100vh;
+  background-color: $black-900-default-theme;
+  @include flex-column;
+  justify-content: center;
+  padding: 1.5rem 2rem;
+
+  @media (min-width: 640px) {
+    padding: 2rem 1, 5rem;
+  }
+
+  .container {
+    @include flex-column;
+    width: 100%;
+    max-width: 28rem;
+    margin: 0 auto;
+
+    .header {
+      @include flex-center;
+      @include flex-column;
+      margin-bottom: 2rem;
+
+      h1 {
+        text-align: center;
+      }
+    }
+
+    .form_content {
+      @include card;
+    }
+  }
+}
+</style>
